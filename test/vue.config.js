722f9782c921fc54ae59aace7eca93bff23055cb
angela <angela52799@gmail.com>
2019-11-17 13:57:09 +0800

    Fix  Network can not connect issue

--- test/vue.config.js
+++ test/vue.config.js
@@ -5,7 +5,6 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     open: process.platform === "darwin",
-    host: "0.0.0.0",
     port: 8000,
     https: false,
     hotOnly: false,
